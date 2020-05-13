2e439586e69e4c851455c92a1201c488048d354d
SylvainParcollet <64834974+SylvainParcollet@users.noreply.github.com>
2020-05-13 11:38:28 +0200

    Update boxk_sps.js

--- boxk_sps.js
+++ boxk_sps.js
@@ -15,7 +15,7 @@
 		</form>
 	`;
 
-	class BoxSps extends HTMLElement {
+	class BoxkSps extends HTMLElement {
 		constructor() {
 			super();
 			this._shadowRoot = this.attachShadow({mode: "open"});
@@ -43,4 +43,4 @@
 		}
 	}
 
-customElements.define("com-demo-boxk-sps", BoxkSps);
+customElements.define("com-demok-box-sps", BoxkSps);
